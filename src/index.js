acb53037378de9d0e8b57ddd61f20f2116602243
Arseniy <krutoi.arseniy@gmail.com>
2024-08-13 20:27:09 +0300

    init: start revers-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    n = String(n);
+    const arrRevers = n.split("");
+    console.log(arrRevers);
+    arrRevers.reverse();
+    let arrNumber = arrRevers.join("");
+    arrNumber = Number(arrNumber);
+    return arrNumber;
+
 }
+
+const ff = reverse(123);
+
+console.log(ff);
