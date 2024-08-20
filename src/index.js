e5349abd463154f5d65155a898db6b5d68f426dd
Arseniy <krutoi.arseniy@gmail.com>
2024-08-20 21:14:17 +0300

    feat: finish

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
     n = String(n);
     const arrRevers = n.split("");
-    console.log(arrRevers);
     arrRevers.reverse();
     let arrNumber = arrRevers.join("");
-    arrNumber = Number(arrNumber);
+    arrNumber = parseInt(arrNumber);
     return arrNumber;
-
 }
-
-const ff = reverse(123);
-
-console.log(ff);
